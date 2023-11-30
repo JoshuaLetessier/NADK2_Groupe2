f58e8d82a3e2d3114c84d26f21c62cd5040072fd
JoshuaLetessier <jletessier@gaming.tech>
2023-11-30 17:36:11 +0100

    scene de base

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-window.addEventListener('load', initApp);
-
-async function initApp() {
+async function InitApp() {
   await SDK3DVerse.joinOrStartSession({
-    userToken: 'PUBLIC_TOKEN',
-    sceneUUID: 'SCENE_UUID',
-    canvas: document.getElementById('display-canvas'),
+    userToken: "public_VgFOTMCc8tJRIuf2",
+    sceneUUID: "f8eb08e3-c82f-4775-9099-9df75861dc16",
+    canvas: document.getElementById("display-canvas"),
     viewportProperties: {
       defaultControllerType: SDK3DVerse.controller_type.orbit,
     },
   });
 }
+
+window.addEventListener('load', InitApp());
