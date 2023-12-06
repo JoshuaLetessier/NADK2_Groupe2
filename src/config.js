2907a4e58833761490098d825f8ba4197e3c196b
JoshuaLetessier <jletessier@gaming.tech>
2023-12-06 15:35:36 +0100

    josh

--- src/config.js
+++ src/config.js
@@ -1,3 +1,9 @@
-export const publicToken = "public_VgFOTMCc8tJRIuf2";
-export const mainSceneUUID = "f8eb08e3-c82f-4775-9099-9df75861dc16";
-export const characterControllerSceneUUID = "73461dd6-f66a-45cf-a53a-3239fcca25a9";
+export const publicToken = "public_qYKytkFTB0gWMxry";
+export const mainSceneUUID = "a14fe655-9417-4bb0-9d63-af346ee6ad7e";
+export const characterControllerSceneUUID = "56cd70af-4daa-4922-b972-ecfcfe0f4943";
+export const level = "a14fe655-9417-4bb0-9d63-af346ee6ad7e";
+
+//entity
+
+export const characterEntity =  "3abdfe77-a25c-4a11-8b36-da651d1e21c7";
+export const objectEntity = "5ed7522a-a31c-41e6-98f4-35a7e900a596";
