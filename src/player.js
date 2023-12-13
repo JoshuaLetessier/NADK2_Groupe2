8f04b028de3d4c8f53211fb24c83e9c712c6d8f8
Quentinjecte <qrenaud@gaming.tech>
2023-12-13 10:48:40 +0100

    test block

--- src/player.js
+++ src/player.js
@@ -30,16 +30,13 @@ export async function InitFirstPersonController(charCtlSceneUUID) {
     playerSceneEntity.setComponent('local_transform', { position: [-3, 0, 0] })
 
     const player = await SDK3DVerse.engineAPI.findEntitiesByNames("Dieux Quentin");
-
     const block = await SDK3DVerse.engineAPI.findEntitiesByEUID({ value: "779b6587-629c-428d-aa8a-423c9709dc94" });
+
 
     SDK3DVerse.engineAPI.onEnterTrigger(async (player, block) => {
         console.log(player/*.components.debug_name.value*/, " entered trigger of ", block.components.debug_name.value);
 
-        setInterval(function () {
-            follow(block);
-
-        }, 10);
+        console.log("appuyer sur A pour prendre l'objet");
         //surbrillance de l'object
         SDK3DVerse.engineAPI.selectEntities([block]);
     });
@@ -57,12 +54,32 @@ export async function InitFirstPersonController(charCtlSceneUUID) {
     SDK3DVerse.setMainCamera(firstPersonCamera);
 }   
 
-async function follow(object) {
+async function Drop(object) {
 
     //object.setGlobalTransform(transformCamera[0].getTransform());
-    const transformCamera = await SDK3DVerse.engineAPI.cameraAPI.getActiveViewports();
 
-    object.setGlobalTransform({ position: [transformCamera[0].getTransform().position[0], transformCamera[0].getTransform().position[1] - 1, transformCamera[0].getTransform().position[2]], orientation: [0, 0, 0, 1], scale: [0.5, 0.5, 0.5] });
-    
+    object.local_transform({ position: [transformCamera[0].getTransform().position[0], transformCamera[0].getTransform().position[1] - 2, transformCamera[0].getTransform().position[2]], orientation: [0, 0, 0, 1], scale: [0.5, 0.5, 0.5] });
+
     //console.log(object.getGlobalTransform());
 }
+
+async function handleKeyDown(event) {
+
+    const block = await SDK3DVerse.engineAPI.findEntitiesByEUID({ value: "779b6587-629c-428d-aa8a-423c9709dc94" });
+    const transformCamera = await SDK3DVerse.engineAPI.cameraAPI.getActiveViewports();
+
+    var keyPressed = event.key;
+
+    if (keyPressed == "a") { //récupérer object
+        setInterval(function () {
+
+            block.setGlobalTransform({ position: [transformCamera[0].getTransform().position[0], transformCamera[0].getTransform().position[1] - 1, transformCamera[0].getTransform().position[2]], orientation: [0, 0, 0, 1], scale: [0.5, 0.5, 0.5] });
+
+        }, 10);
+    }
+    else if (keyPressed == "p") //pose du bloque
+    {
+        Drop(block);
+    }
+}
+document.addEventListener("keydown", handleKeyDown);
