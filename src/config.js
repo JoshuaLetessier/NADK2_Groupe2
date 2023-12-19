f796131bf3f63c77fec990659afe68754228a3fa
Quentinjecte <qrenaud@gaming.tech>
2023-12-19 16:32:47 +0100

    ok

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const publicToken = "public_qYKytkFTB0gWMxry";
-export const mainSceneUUID = "a14fe655-9417-4bb0-9d63-af346ee6ad7e";
-export const characterControllerSceneUUID = "432bca42-efa1-490d-a56d-d447aaf93688";
+export const publicToken = "public_9puwL1hBazpRxiUI";
+export const mainSceneUUID = "7ea789e8-bfe6-4b92-ac12-2b3168ddca9f";
+export const characterControllerSceneUUID = "36fb6a2d-4909-4281-ae23-a23d0c9f83e1";
